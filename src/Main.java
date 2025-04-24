8df0090fd33874f60e435bca14f7c8a285296499
DiegoVillanueva32 <d.villanueva0504@gmail.com>
2025-04-23 20:23:05 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main( String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         System.out.println("Hello, What's your name?");
         String customerName = input.next();
-        System.out.println("Nice to meet you "+ customerName);
+        System.out.println("Nice to meet you" + customerName);
 
         String path = "./welcome.txt";
-        FileReader reader = new FileReader(path);
-        while (reader.ready()) {
-            System.out.println((char) reader.read());
+        FileReader reader= new FileReader(path);
+        while (reader.ready()){
+            System.out.print((char)reader.read());
         }
+
         reader.close();
+
+        FileWriter output = new FileWriter("./customerReview.txt");
+        System.out.println("\nPlease, provide a review: ");
+        String review = input.next();
+        String customerReview = customerName + ":" + review;
+        output.write(customerReview);
+        output.close();
     }
-}
+}.}
